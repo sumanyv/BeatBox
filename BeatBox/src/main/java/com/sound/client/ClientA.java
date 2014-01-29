287885d3dd4c64180828ed16534b4e745f6e9c9f
Aditya <adityasekhar@outlook.com>
2014-01-29 17:25:05 +0530

    Update with slf4j - log4j logger

--- BeatBox/src/main/java/com/sound/client/ClientA.java
+++ BeatBox/src/main/java/com/sound/client/ClientA.java
@@ -2,18 +2,23 @@ package com.sound.client;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sound.service.Player;
 
 public class ClientA {
 	
+	private static final Logger log = LoggerFactory.getLogger(ClientA.class);
+	
 	public static void main(String[] args){
-		
+		log.debug("Started ClientA ");
 		Player player = new Player();
 		try {
 			player.startPlayer();
 		} catch (MidiUnavailableException | InvalidMidiDataException e) {
-			System.out.println(e.getMessage());
+			log.info(e.getMessage());
+			e.printStackTrace();
 		}finally{
 		}
 	}
--- BeatBox/src/main/java/com/sound/service/Player.java
+++ BeatBox/src/main/java/com/sound/service/Player.java
@@ -9,18 +9,22 @@ import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Player {
+	
+	private static final Logger log = LoggerFactory.getLogger(Player.class);
 
 	public final void startPlayer() throws MidiUnavailableException, InvalidMidiDataException{
 		
 		Sequencer p = MidiSystem.getSequencer();
 		p.open();
-		
+		log.debug("Player Started");
 		Sequence seq = new Sequence(Sequence.PPQ,4);
 		Track track = seq.createTrack();
 		
 		for(int i=5;i<61;i+=4){
-			
 			track.add(makeEvent(144, 1, i, 100, i));
 			track.add(makeEvent(128, 1, i, 100, i+2));
 		}
@@ -32,12 +36,10 @@ public class Player {
 
 	private static MidiEvent makeEvent(int comd,int chan ,int one ,int two ,int tick) throws InvalidMidiDataException{
 		MidiEvent event = null;
-		
 		ShortMessage sMsg = new ShortMessage();
 		sMsg.setMessage(comd, chan, one, two);
-		
-		event = new MidiEvent(sMsg,tick);
-		
+		event = new MidiEvent(sMsg,tick); 
+		log.trace("Event Details : command {} : channel {} : data1 {} : data2 {} : tick {}",comd,chan,one,two,tick);
 		return event;
 		
 	}
